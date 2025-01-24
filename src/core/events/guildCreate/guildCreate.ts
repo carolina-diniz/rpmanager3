c193bea5d6818a35ec900be71e86213726a62c3f
Carolina Diniz <caroldinizc21@gmail.com>
2025-01-24 17:31:13 -0300

    fix(core/events): arruma os comandos ao adicionar bot ao servidor

--- src/core/events/guildCreate/guildCreate.ts
+++ src/core/events/guildCreate/guildCreate.ts
@@ -1,11 +1,16 @@
 import { Guild } from "discord.js";
 import print from "../../print/print";
 import guildService from "../services/guild.service";
+import { client } from "../../../connections";
+import deployCommands from "../../deployCommands/deployCommands";
 
 export default async (guild: Guild) => {
   print.init(__filename);
+  const { id, name } = guild;
   
   await guildService.createAtDatabase(guild);
+
+  await deployCommands(client.user!.id, { id, name });
 
   print.log(__filename, `creating new guild: ${guild.name}`, guild);
 };
